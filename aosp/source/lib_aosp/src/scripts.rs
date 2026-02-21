2bf293d29d8a1368ddbd7f88fe75af349c41b52e
._______166 <retromac9x2@gmail.com>
2026-02-21 19:39:50 +0000

    use latest graphene tag to get aosp tag and branch

--- aosp/source/lib_aosp/src/scripts.rs
+++ aosp/source/lib_aosp/src/scripts.rs
@@ -18,9 +18,7 @@ fn fetch_common_sh_at_tag(tag: &str) -> String {
 }
 
 pub fn read_common_sh() -> (String, String, String) {
-    let tag_old = graphene_tag_from_manifest("../../platform_manifest/default.xml");
-
-    let contents = fetch_common_sh_at_tag(&tag_old);
+    let contents = fetch_common_sh_at_tag(latest_graphene_tag());
 
     let mut aosp_tag = String::new();
     let mut aosp_tag_old = String::new();
