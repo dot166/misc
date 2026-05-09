2362010fc81ff08515cdba5424e11293751da2fe
._______166 <retromac9x2@gmail.com>
2026-05-09 21:30:50 +0100

    nexus fixes

--- nexus/java/io/github/dot166/nexus/NexusOverlay.kt
+++ nexus/java/io/github/dot166/nexus/NexusOverlay.kt
@@ -1,11 +1,17 @@
 package io.github.dot166.nexus
 
-import android.app.LocalActivityManagerCompat
+import android.app.ActivityOptions
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Display
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
@@ -25,18 +31,12 @@ class NexusOverlay(
     android.R.style.Theme_Translucent_NoTitleBar
 ), LifecycleOwner {
 
-    companion object {
-        private const val KEY_ACTIVITY_STATE = "activity_state"
-    }
-
     private val lifecycleRegistry by lazy {
         LifecycleRegistry(this)
     }
 
     override val lifecycle
         get() = lifecycleRegistry
-
-    private val activityManager = LocalActivityManagerCompat(context, true)
     private var backgroundBlurProgress = 0f
     private val blurProvider = BlurProvider(resources)
     private var isResumed = false
@@ -53,23 +53,59 @@ class NexusOverlay(
             it.navigationBarColor = Color.TRANSPARENT
         }
         lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_CREATE)
-        activityManager.dispatchCreate(bundle?.getBundle(KEY_ACTIVITY_STATE) ?: Bundle())
         window?.decorView?.removeStatusNavBackgroundOnPreDraw()
-        val window = activityManager.startActivity(
-            "overlay", Intent("io.github.dot166.nexus.WINDOW_OVERLAY_PROVIDER").apply {
-                component =
-                    ComponentName.unflattenFromString(
-                        PreferenceManager.getDefaultSharedPreferences(
-                            this@NexusOverlay
-                        ).getString(
-                            "feed_provider",
-                            "io.github.dot166.nexus/io.github.dot166.nexus.DefaultStub"
-                        ) ?: "io.github.dot166.nexus/io.github.dot166.nexus.DefaultStub" // return stub activity when not set or null, prevents user confusion
+        val surface = SurfaceView(this)
+        val dm = getSystemService(DISPLAY_SERVICE) as DisplayManager
+        val metrics = DisplayMetrics()
+        dm.getDisplay(Display.DEFAULT_DISPLAY).getRealMetrics(metrics)
+        var vd: VirtualDisplay? = null
+        surface.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                val surfaceWidth = holder.surfaceFrame.width()
+                val surfaceHeight = holder.surfaceFrame.height()
+                vd =
+                    dm.createVirtualDisplay(
+                        "overlay",
+                        surfaceWidth,
+                        surfaceHeight,
+                        metrics.densityDpi,
+                        holder.surface,
+                        DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC or DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY
                     )
-            },
-            this
-        )
-        container?.addView(window.decorView.removeStatusNavBackgroundOnPreDraw())
+                val options = ActivityOptions.makeBasic()
+                options.setLaunchDisplayId(vd.display.displayId)
+                val intent: Intent =
+                    Intent("io.github.dot166.nexus.WINDOW_OVERLAY_PROVIDER").apply {
+                        component =
+                            ComponentName.unflattenFromString(
+                                PreferenceManager.getDefaultSharedPreferences(
+                                    this@NexusOverlay
+                                ).getString(
+                                    "feed_provider",
+                                    "io.github.dot166.nexus/io.github.dot166.nexus.DefaultStub"
+                                )
+                                    ?: "io.github.dot166.nexus/io.github.dot166.nexus.DefaultStub" // return stub activity when not set or null, prevents user confusion
+                            )
+                    }
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent, options.toBundle())
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                vd?.resize(width, height, metrics.densityDpi)
+                vd?.surface = holder.surface
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                vd?.release()
+            }
+        })
+        container?.addView(surface.removeStatusNavBackgroundOnPreDraw())
     }
 
     override fun onPause() {
@@ -77,14 +113,12 @@ class NexusOverlay(
         isResumed = false
         super.onPause()
         lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_PAUSE)
-        activityManager.dispatchPause(false)
     }
 
     override fun onDestroy(isFinishing: Boolean) {
         onPause()
         super.onDestroy(isFinishing)
         lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_DESTROY)
-        activityManager.dispatchDestroy(isFinishing)
     }
 
     override fun onStop() {
@@ -104,18 +138,12 @@ class NexusOverlay(
         isResumed = true
         super.onResume()
         lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_RESUME)
-        activityManager.dispatchResume()
-        updateProgressViews(backgroundBlurProgress, true)
+        //updateProgressViews(backgroundBlurProgress, true)
     }
 
     override fun onDragProgress(progress: Float) {
         super.onDragProgress(progress)
-        updateProgressViews(progress)
-    }
-
-    override fun onSaveInstanceState(bundle: Bundle) {
-        super.onSaveInstanceState(bundle)
-        bundle.putBundle(KEY_ACTIVITY_STATE, activityManager.saveInstanceState())
+        //updateProgressViews(progress)
     }
 
     private fun updateProgressViews(progress: Float, force: Boolean = false) {
