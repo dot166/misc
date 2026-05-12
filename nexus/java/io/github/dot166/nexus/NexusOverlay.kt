f41154ed3d1709271732e4937808bc07aaa90d30
._______166 <retromac9x2@gmail.com>
2026-05-12 22:35:37 +0100

    fixes
    mostly works now

--- nexus/java/io/github/dot166/nexus/NexusOverlay.kt
+++ nexus/java/io/github/dot166/nexus/NexusOverlay.kt
@@ -1,24 +1,24 @@
 package io.github.dot166.nexus
 
+import android.app.ActivityManager
 import android.app.ActivityOptions
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.SurfaceTexture
 import android.hardware.display.DisplayManager
 import android.hardware.display.VirtualDisplay
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.Display
-import android.view.SurfaceHolder
-import android.view.SurfaceView
+import android.view.Surface
+import android.view.TextureView
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.core.view.doOnPreDraw
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LifecycleRegistry
 import androidx.preference.PreferenceManager
 import com.google.android.gsa.overlay.controllers.OverlayController
 
@@ -29,17 +29,14 @@ class NexusOverlay(
     context,
     0,
     android.R.style.Theme_Translucent_NoTitleBar
-), LifecycleOwner {
+) {
 
-    private val lifecycleRegistry by lazy {
-        LifecycleRegistry(this)
-    }
-
-    override val lifecycle
-        get() = lifecycleRegistry
     private var backgroundBlurProgress = 0f
     private val blurProvider = BlurProvider(resources)
     private var isResumed = false
+    private var lockOn = false
+    private var lockOff = true
+    private var vd: VirtualDisplay? = null
 
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
@@ -52,98 +49,138 @@ class NexusOverlay(
             it.statusBarColor = Color.TRANSPARENT
             it.navigationBarColor = Color.TRANSPARENT
         }
-        lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_CREATE)
         window?.decorView?.removeStatusNavBackgroundOnPreDraw()
-        val surface = SurfaceView(this)
-        val dm = getSystemService(DISPLAY_SERVICE) as DisplayManager
-        val metrics = DisplayMetrics()
-        dm.getDisplay(Display.DEFAULT_DISPLAY).getRealMetrics(metrics)
-        var vd: VirtualDisplay? = null
-        surface.holder.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceCreated(holder: SurfaceHolder) {
-                val surfaceWidth = holder.surfaceFrame.width()
-                val surfaceHeight = holder.surfaceFrame.height()
-                vd =
-                    dm.createVirtualDisplay(
-                        "overlay",
-                        surfaceWidth,
-                        surfaceHeight,
-                        metrics.densityDpi,
-                        holder.surface,
-                        DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC or DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY
-                    )
-                val options = ActivityOptions.makeBasic()
-                options.setLaunchDisplayId(vd.display.displayId)
-                val intent: Intent =
-                    Intent("io.github.dot166.nexus.WINDOW_OVERLAY_PROVIDER").apply {
-                        component =
-                            ComponentName.unflattenFromString(
-                                PreferenceManager.getDefaultSharedPreferences(
-                                    this@NexusOverlay
-                                ).getString(
-                                    "feed_provider",
-                                    "io.github.dot166.nexus/io.github.dot166.nexus.DefaultStub"
-                                )
-                                    ?: "io.github.dot166.nexus/io.github.dot166.nexus.DefaultStub" // return stub activity when not set or null, prevents user confusion
-                            )
-                    }
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent, options.toBundle())
-            }
-
-            override fun surfaceChanged(
-                holder: SurfaceHolder,
-                format: Int,
-                width: Int,
-                height: Int
-            ) {
-                vd?.resize(width, height, metrics.densityDpi)
-                vd?.surface = holder.surface
-            }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder) {
-                vd?.release()
-            }
-        })
-        container?.addView(surface.removeStatusNavBackgroundOnPreDraw())
     }
 
     override fun onPause() {
         if (!isResumed) return
         isResumed = false
         super.onPause()
-        lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_PAUSE)
     }
 
     override fun onDestroy(isFinishing: Boolean) {
         onPause()
         super.onDestroy(isFinishing)
-        lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_DESTROY)
     }
 
     override fun onStop() {
         onPause()
         super.onStop()
-        lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_STOP)
         blurProvider.applyBlurToWindow(window!!, 0f)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_START)
     }
 
     override fun onResume() {
         if (isResumed) return
         isResumed = true
         super.onResume()
-        lifecycleRegistry.handleLifecycleEventSafely(Lifecycle.Event.ON_RESUME)
-        //updateProgressViews(backgroundBlurProgress, true)
+        updateProgressViews(backgroundBlurProgress, true)
     }
 
     override fun onDragProgress(progress: Float) {
         super.onDragProgress(progress)
-        //updateProgressViews(progress)
+        updateProgressViews(progress)
+        Log.i("AAAAAA", progress.toString())
+        if (progress == 1.0f) {
+            if (!lockOn) {
+                lockOn = true
+                lockOff = false
+                val surface = TextureView(this)
+                surface.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+                    override fun onSurfaceTextureAvailable(
+                        surface: SurfaceTexture,
+                        width: Int,
+                        height: Int
+                    ) {
+                        val dm = getSystemService(DISPLAY_SERVICE) as DisplayManager
+                        val metrics = DisplayMetrics()
+                        dm.getDisplay(Display.DEFAULT_DISPLAY).getRealMetrics(metrics)
+                        vd =
+                            dm.createVirtualDisplay(
+                                "overlay",
+                                width,
+                                height,
+                                metrics.densityDpi,
+                                Surface(surface),
+                                DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC or DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY
+                            )
+                        val options = ActivityOptions.makeBasic()
+                        options.setLaunchDisplayId(vd!!.display.displayId)
+                        val intent: Intent =
+                            Intent("io.github.dot166.nexus.WINDOW_OVERLAY_PROVIDER").apply {
+                                component =
+                                    ComponentName.unflattenFromString(
+                                        PreferenceManager.getDefaultSharedPreferences(
+                                            this@NexusOverlay
+                                        ).getString(
+                                            "feed_provider",
+                                            "io.github.dot166.nexus/io.github.dot166.nexus.DefaultStub"
+                                        )
+                                            ?: "io.github.dot166.nexus/io.github.dot166.nexus.DefaultStub" // return stub activity when not set or null, prevents user confusion
+                                    )
+                            }
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                        startActivity(intent, options.toBundle())
+                    }
+
+                    override fun onSurfaceTextureSizeChanged(
+                        surface: SurfaceTexture,
+                        width: Int,
+                        height: Int
+                    ) {
+                        if (vd == null) {
+                            return
+                        }
+                        val dm = getSystemService(DISPLAY_SERVICE) as DisplayManager
+                        val metrics = DisplayMetrics()
+                        dm.getDisplay(Display.DEFAULT_DISPLAY).getRealMetrics(metrics)
+                        vd!!.resize(width, height, metrics.densityDpi)
+                    }
+
+                    override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                        if (vd == null) {
+                            return true
+                        }
+                        val am = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+                        val tasks = am.getAppTasks()
+
+                        for (task in tasks) {
+                            val info = task.taskInfo
+                            if (info.displayId == vd!!.display.displayId) {
+                                task.finishAndRemoveTask()
+                            }
+                        }
+                        vd!!.release()
+                        vd = null
+                        return true
+                    }
+
+                    override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+                    }
+                }
+                container?.addView(surface.removeStatusNavBackgroundOnPreDraw())
+            }
+        } else if (progress == 0.0f) {
+            if (!lockOff) {
+                lockOff = true
+                lockOn = false
+                if (vd == null) {
+                    container?.removeAllViews() // nuke it
+                    return
+                }
+                val am = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+                val tasks = am.getAppTasks()
+
+                for (task in tasks) {
+                    val info = task.taskInfo
+                    if (info.displayId == vd!!.display.displayId) {
+                        task.finishAndRemoveTask()
+                    }
+                }
+                vd!!.release()
+                vd = null
+                container?.removeAllViews() // nuke it
+            }
+        }
     }
 
     private fun updateProgressViews(progress: Float, force: Boolean = false) {
@@ -151,14 +188,6 @@ class NexusOverlay(
         backgroundBlurProgress = progress
         container?.background = null
         blurProvider.applyBlurToWindow(window!!, progress)
-    }
-
-    fun LifecycleRegistry.handleLifecycleEventSafely(event: Lifecycle.Event) {
-        try {
-            handleLifecycleEvent(event)
-        } catch (e: IllegalStateException) {
-            //Already at this event
-        }
     }
 
     fun View.removeStatusNavBackgroundOnPreDraw() = apply {
